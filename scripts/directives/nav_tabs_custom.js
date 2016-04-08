49962b2e1fb80531bbb2621f2302657112f9deb3
Stjepan Obrankovic <stjepano@gmail.com>
2016-04-08 15:22:51 +0200

    Implement nav-tabs-custom directive

---
+++ scripts/directives/nav_tabs_custom.js
@@ -0,0 +1,31 @@
+angular.module('adminlte')
+    .directive('navTabsCustom', function() {
+        var directive = {
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+                console.log('link called');
+                element.addClass('nav-tabs-custom');
+                element.find('ul.nav > li > a[data-toggle="tab"]').click(function(evt) {
+                    var $ = jQuery;
+                    evt.preventDefault();
+                    var href = $(this).attr('href');
+                    console.log($(this).attr('href'));
+
+                    element.find('div.tab-content > div.tab-pane.active').each(function() {
+                        $(this).removeClass('active');
+                    });
+
+                    element.find('div.tab-content > ' + href).each(function() {
+                        $(this).addClass('active');
+                    });
+
+                    element.find('ul.nav > li.active').each(function() {
+                        $(this).removeClass('active');
+                    });
+                    $(this).parent().addClass('active');
+                });
+            }
+        };
+
+        return directive;
+    });
