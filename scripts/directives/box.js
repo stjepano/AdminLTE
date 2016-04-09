8558a69f04fe72d5849e80bd45c58aba535301f2
Stjepan Obrankovic <stjepano@gmail.com>
2016-04-09 12:40:01 +0200

    Implement buttons page and box-header tag

--- scripts/directives/box.js
+++ scripts/directives/box.js
@@ -10,6 +10,11 @@ angular.module('adminlte')
                 headerClass: '@?',
                 bodyClass: '@?',
             },
+            link: function(scope, element) {
+                if ( angular.isDefined(scope.titleEl) ) {
+                    element.children('div.box').children('div.box-header').append(scope.titleEl);
+                }
+            },
             controller: function($scope) {
                 $scope.type = angular.isDefined($scope.type) ? $scope.type : 'default';
                 $scope.headerBorder = angular.isDefined($scope.headerBorder) ? $scope.headerBorder : true;
@@ -20,6 +25,24 @@ angular.module('adminlte')
             },
             transclude: true,
             templateUrl: 'scripts/templates/box.html'
+        };
+
+        return directive;
+    })
+    .directive('boxHeader', function($sce) {
+        var directive = {
+            require: '^box',
+            controller: function() {
+
+            },
+            link: function(scope, element, attrs, boxCtrl, transclude) {
+                transclude(scope, function(clone) {
+                    scope.$parent.titleEl = clone;
+                });
+            },
+            scope: false,
+            transclude: true,
+            template: ''
         };
 
         return directive;
